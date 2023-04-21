99f757f1bb47886142c79030bc2eff6deacc313f
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-21 18:59:06 +0300

    fin

--- src/entities/Category/index.ts
+++ src/entities/Category/index.ts
@@ -2,3 +2,9 @@ export { CategoryForm } from "./ui/CategoryForm"
 
 export type { CategorySchema } from "./model/types/categorySchema"
 export { categoryReducer, categoryActions } from "./model/slice/categorySlice"
+export {
+    getCategoryName,
+    getCategoryImage,
+    getCategoryParent,
+    getCategorySlug,
+} from "./model/selectors/categorySelectors"
--- src/features/ManageCategory/ui/AddCategory/AddCategory.tsx
+++ src/features/ManageCategory/ui/AddCategory/AddCategory.tsx
@@ -1,5 +1,51 @@
+import {
+    getCategoryName,
+    getCategoryImage,
+    getCategoryParent,
+    getCategorySlug,
+} from "entities/Category"
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { $api } from "shared/api/api"
 import { Button } from "shared/ui/Button/Button"
 
 export function AddCategory() {
-    return <Button>Add new category</Button>
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [isSuccess, setIsSuccess] = useState(false)
+
+    const name = useSelector(getCategoryName)
+    const hru = useSelector(getCategorySlug)
+    const parentId = useSelector(getCategoryParent)
+    const icon = useSelector(getCategoryImage)
+
+    async function clickHandler() {
+        const data = { name, hru, parent_category_id: parentId, icon }
+
+        try {
+            setError("")
+            setIsSuccess(false)
+            setIsLoading(true)
+
+            const response = await $api.post("/category", data)
+
+            if (!response.data) {
+                throw new Error("err")
+            }
+            setIsSuccess(true)
+        } catch (e) {
+            setError("error")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <>
+            {isSuccess && !error && <div>Category added successfully!</div>}
+            {!!error && <div>Oops, something went wrong</div>}
+
+            <Button onClick={clickHandler}>{isLoading ? "Loading..." : "Add New Category"}</Button>
+        </>
+    )
 }
