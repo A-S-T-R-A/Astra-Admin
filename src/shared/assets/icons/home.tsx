5c1f9c1a12bbb635263525e1a60fae31ca8e0767
Sergey Titov <sergeyntitov@gmail.com>
2023-05-25 15:47:44 +0300

    linter corr

--- src/shared/assets/icons/home.tsx
+++ src/shared/assets/icons/home.tsx
@@ -7,7 +7,7 @@ interface IconProps {
     color?: "accent" | "base"
 }
 export default function HomeIcon(props: IconProps) {
-    const { className, size = "L", color = "base" } = props
+    const { className, size = "L", color = "primary" } = props
     return (
         <div
             className={classNames(
@@ -32,10 +32,8 @@ export default function HomeIcon(props: IconProps) {
                 <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 
-                    1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 
-                    1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 
-                    1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                    // eslint-disable-next-line max-len
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
                 />
             </svg>
         </div>
